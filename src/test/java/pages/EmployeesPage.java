6d60ba6bdf0709dac4ae1a872acc9a0454816317
minhvuong220820 <minhvuongys@gmail.com>
2024-07-02 23:05:24 +0700

    upload add employee testcase

--- src/test/java/pages/EmployeesPage.java
+++ src/test/java/pages/EmployeesPage.java
@@ -1,13 +1,14 @@
 package pages;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.List;
 
@@ -27,6 +28,27 @@ public class EmployeesPage {
     private By dataTableInfoLocator = By.xpath("//div[@id='xin_table_info']");
     private By dataTableNextPageButtonLocator = By.xpath("//a[normalize-space()='Next']");
     private By employeesSectionSearchInputLocator = By.xpath("//input[@type='search']");
+
+    private By employeesPageConfirmMessageLocator = By.xpath("//div[@class='toast-message']");
+
+    private By employeesSectionAddNewButtonLocator = By.xpath("//a[normalize-space()='Add New']");
+    private By employeesSectionAddNewEmployeeFormLocator = By.xpath("//div[@id='add_form']");
+    private By employeesSectionAddNewEmployeeFormFirstNameInputLocator = By.xpath("//input[@placeholder='First Name']");
+    private By employeesSectionAddNewEmployeeFormLastNameInputLocator = By.xpath("//input[@placeholder='Last Name']");
+    private By employeesSectionAddNewEmployeeFormEmployeeIDInputLocator = By.xpath("//input[@placeholder='Employee ID']");
+    private By employeesSectionAddNewEmployeeFormContactNumberLocator = By.xpath("//input[@placeholder='Contact Number']");
+    private By employeesSectionAddNewEmployeeFormGenderSelectorLocator = By.xpath("//select[@name='gender']");
+    private By employeesSectionAddNewEmployeeFormEmailInputLocator = By.xpath("//input[@placeholder='Email']");
+    private By employeesSectionAddNewEmployeeFormUserNameInputLocator = By.xpath("//input[@placeholder='Username']");
+    private By employeesSectionAddNewEmployeeFormPasswordInputLocator = By.xpath("//input[@placeholder='Password']");
+    private By employeesSectionAddNewEmployeeFormOfficeShiftSelectorLocator = By.xpath("//select[@name='office_shift_id']");
+    private By employeesSectionAddNewEmployeeFormRoleSelectorLocator = By.xpath("//select[@name='role']");
+    private By employeesSectionAddNewEmployeeFormDepartmentSelectorLocator = By.xpath("//select[@id='department_id']");
+    private By employeesSectionAddNewEmployeeFormDesignationSelectorLocator = By.xpath("//select[@name='designation_id']");
+    private By employeesSectionAddNewEmployeeFormBasicSalaryInputLocator = By.xpath("//input[@placeholder='Gross Salary']");
+    private By employeesSectionAddNewEmployeeFormHourlyRateInputLocator = By.xpath("//input[@placeholder='Hourly Rate']");
+    private By employeesSectionAddNewEmployeeFormProfilePictureInputLocator = By.xpath("//input[@name='file']");
+    private By employeesSectionAddNewEmployeeFormSaveButtonLocator = By.xpath("//div[@class='card-footer text-right']//button[@type='submit']");
 
     public EmployeesPage(WebDriver driver) {
         this.driver = driver;
@@ -111,5 +133,178 @@ public class EmployeesPage {
         verifyDataInTableByQuery(itemsPerPage, query);
     }
 
+    public void clickAddNewButton() {
+        WebElement employeesSectionAddNewButton = driver.findElement(employeesSectionAddNewButtonLocator);
+        employeesSectionAddNewButton.click();
 
+        WebElement employeesSectionAddNewEmployeeForm = driver.findElement(employeesSectionAddNewEmployeeFormLocator);
+        assertTrue(employeesSectionAddNewEmployeeForm.isDisplayed(), "Add new employee form ko duoc hien thi.");
+    }
+
+    public void enterFirstName(String firstName) {
+        WebElement employeesSectionAddNewEmployeeFormFirstNameInput = driver.findElement(employeesSectionAddNewEmployeeFormFirstNameInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormFirstNameInput.isDisplayed(), "First name input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormFirstNameInput.isEnabled(), "First name input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormFirstNameInput.sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastName) {
+        WebElement employeesSectionAddNewEmployeeFormLastNameInput = driver.findElement(employeesSectionAddNewEmployeeFormLastNameInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormLastNameInput.isDisplayed(), "Last name input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormLastNameInput.isEnabled(), "Last name input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormLastNameInput.sendKeys(lastName);
+    }
+
+    public void clearAndEnterEmployeeID(String employeeID) {
+        WebElement employeesSectionAddNewEmployeeFormEmployeeIDInput = driver.findElement(employeesSectionAddNewEmployeeFormEmployeeIDInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormEmployeeIDInput.isDisplayed(), "Employee ID input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormEmployeeIDInput.isEnabled(), "Employee ID input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormEmployeeIDInput.clear();
+        employeesSectionAddNewEmployeeFormEmployeeIDInput.sendKeys(employeeID);
+    }
+
+    public void enterContactNumber(String contactNumber) {
+        WebElement employeesSectionAddNewEmployeeFormContactNumber = driver.findElement(employeesSectionAddNewEmployeeFormContactNumberLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormContactNumber.isDisplayed(), "Contact number input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormContactNumber.isEnabled(), "Contact number input ko the tuong tac.");
+        employeesSectionAddNewEmployeeFormContactNumber.sendKeys(contactNumber);
+    }
+
+    public void selectGender(String gender) {
+        Select employeesSectionAddNewEmployeeFormGenderSelector = new Select(driver.findElement(employeesSectionAddNewEmployeeFormGenderSelectorLocator));
+        try{
+            employeesSectionAddNewEmployeeFormGenderSelector.selectByVisibleText(gender);
+        }
+        catch(Exception e){
+            System.out.println("Gioi tinh khong ton tai.");
+        }
+    }
+
+    public void enterEmailInput(String email) {
+        WebElement employeesSectionAddNewEmployeeFormEmailInput = driver.findElement(employeesSectionAddNewEmployeeFormEmailInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormEmailInput.isDisplayed(), "Email input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormEmailInput.isEnabled(), "Email input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormEmailInput.sendKeys(email);
+    }
+
+    public void enterUserNameInput(String username) {
+        WebElement employeesSectionAddNewEmployeeFormUserNameInput = driver.findElement(employeesSectionAddNewEmployeeFormUserNameInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormUserNameInput.isDisplayed(), "Username input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormUserNameInput.isEnabled(), "Username input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormUserNameInput.sendKeys(username);
+    }
+
+    public void enterPasswordInput(String password) {
+        WebElement employeesSectionAddNewEmployeeFormPasswordInput = driver.findElement(employeesSectionAddNewEmployeeFormPasswordInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormPasswordInput.isDisplayed(), "Password input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormPasswordInput.isEnabled(), "Password input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormPasswordInput.sendKeys(password);
+    }
+
+    public void selectOfficeShift(String officeShift) {
+        Select employeesSectionAddNewEmployeeFormOfficeShiftSelector = new Select(driver.findElement(employeesSectionAddNewEmployeeFormOfficeShiftSelectorLocator));
+        try{
+            employeesSectionAddNewEmployeeFormOfficeShiftSelector.selectByVisibleText(officeShift);
+        }
+        catch(Exception e){
+            System.out.println("Office Shift ko ton tai.");
+        }
+    }
+
+    public void selectRole(String role) {
+        Select employeesSectionAddNewEmployeeFormRoleSelector = new Select(driver.findElement(employeesSectionAddNewEmployeeFormRoleSelectorLocator));
+        try{
+            employeesSectionAddNewEmployeeFormRoleSelector.selectByVisibleText(role);
+        }
+        catch(Exception e){
+            System.out.println("Role ko ton tai.");
+        }
+    }
+
+    public void selectDepartment(String department) {
+        Select employeesSectionAddNewEmployeeFormDepartmentSelector = new Select(driver.findElement(employeesSectionAddNewEmployeeFormDepartmentSelectorLocator));
+        try{
+            employeesSectionAddNewEmployeeFormDepartmentSelector.selectByVisibleText(department);
+        }
+        catch(Exception e){
+            System.out.println("Department ko ton tai.");
+        }
+    }
+
+    public void selectDesignation(String designation) {
+        Select employeesSectionAddNewEmployeeFormDesignationSelector = new Select(driver.findElement(employeesSectionAddNewEmployeeFormDesignationSelectorLocator));
+        try{
+            employeesSectionAddNewEmployeeFormDesignationSelector.selectByVisibleText(designation);
+        }
+        catch(Exception e){
+            System.out.println("Designation ko ton tai.");
+        }
+    }
+
+    public void clearAndEnterBasicSalary(String basicSalary) {
+        WebElement employeesSectionAddNewEmployeeFormBasicSalaryInput = driver.findElement(employeesSectionAddNewEmployeeFormBasicSalaryInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormBasicSalaryInput.isDisplayed(), "Basic salary input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormBasicSalaryInput.isEnabled(), "Basic salary input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormBasicSalaryInput.clear();
+        employeesSectionAddNewEmployeeFormBasicSalaryInput.sendKeys(basicSalary);
+    }
+
+    public void clearAndEnterHourlyRate(String hourlyRate) {
+        WebElement employeesSectionAddNewEmployeeFormHourlyRateInput = driver.findElement(employeesSectionAddNewEmployeeFormHourlyRateInputLocator);
+        assertTrue(employeesSectionAddNewEmployeeFormHourlyRateInput.isDisplayed(), "Hourly rate input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormHourlyRateInput.isEnabled(), "Hourly rate input khong the tuong tac.");
+        employeesSectionAddNewEmployeeFormHourlyRateInput.clear();
+        employeesSectionAddNewEmployeeFormHourlyRateInput.sendKeys(hourlyRate);
+    }
+
+    public void enterProfilePicture(String profilePicture) {
+        WebElement employeesSectionAddNewEmployeeFormProfilePictureInput = driver.findElement(employeesSectionAddNewEmployeeFormProfilePictureInputLocator);
+        action.moveToElement(employeesSectionAddNewEmployeeFormProfilePictureInput).perform();
+        //assertTrue(employeesSectionAddNewEmployeeFormProfilePictureInput.isDisplayed(), "Profile picture input ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormProfilePictureInput.isEnabled(), "Profile picture input khong the tuong tac.");
+        try {
+            employeesSectionAddNewEmployeeFormProfilePictureInput.sendKeys(System.getProperty("user.dir") + "/src/test/java/resource/" + profilePicture);
+        }
+        catch (Exception e) {
+            System.out.println("File ko ton tai.");
+        }
+    }
+
+    public void clickSaveButton() {
+        WebElement employeesSectionAddNewEmployeeFormSaveButton = driver.findElement(employeesSectionAddNewEmployeeFormSaveButtonLocator);
+        action.moveToElement(employeesSectionAddNewEmployeeFormSaveButton).perform();
+        assertTrue(employeesSectionAddNewEmployeeFormSaveButton.isDisplayed(), "Save button ko duoc hien thi.");
+        assertTrue(employeesSectionAddNewEmployeeFormSaveButton.isEnabled(), "Save button ko the tuong tac.");
+        employeesSectionAddNewEmployeeFormSaveButton.click();
+    }
+
+    public void verifyConfirmMessage(String confirmMessage) {
+        largeWait.until(ExpectedConditions.visibilityOfElementLocated(employeesPageConfirmMessageLocator));
+        WebElement employeesPageConfirmMessage = driver.findElement(employeesPageConfirmMessageLocator);
+        assertEquals(employeesPageConfirmMessage.getText(), confirmMessage, "Add new employee confirm hien thi sai.");
+    }
+
+    public void addNewEmployee(String firstName, String lastName, String employeeID, String contactNumber, String gender, String email, String username, String password, String officeShift, String role, String department, String designation, String basicSalary, String hourlyRate, String profilePicture, String confirmMessage) throws Exception {
+        viewEmployeesSection();
+        clickAddNewButton();
+        enterFirstName(firstName);
+        enterLastName(lastName);
+        clearAndEnterEmployeeID(employeeID);
+        enterContactNumber(contactNumber);
+        selectGender(gender);
+        enterEmailInput(email);
+        enterUserNameInput(username);
+        enterPasswordInput(password);
+        selectOfficeShift(officeShift);
+        selectRole(role);
+        selectDepartment(department);
+        Thread.sleep(200);
+        selectDesignation(designation);
+        clearAndEnterBasicSalary(basicSalary);
+        clearAndEnterHourlyRate(hourlyRate);
+        enterProfilePicture(profilePicture);
+        clickSaveButton();
+        verifyConfirmMessage(confirmMessage);
+
+    }
 }
--- src/test/java/test/EmployeesTestCase.java
+++ src/test/java/test/EmployeesTestCase.java
@@ -44,6 +44,248 @@ public class EmployeesTestCase extends Initiation {
         employeesPage.searchInEmployeesSection(10, "0 @");
     }
 
+    @Test
+    public void testAddNewEmployeeSuccess() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "1", "Male", "a1@r.com", "qwe123!", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "2.jpg", "Employee added.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyFirstName() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("", "last name", "?", "1", "Male", "a2@r.com", "qwe123!", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The first name field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyLastName() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "", "?", "1", "Male", "a3@r.com", "qwe123!", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The last name field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyEmployeeID() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "", "1", "Male", "a4@r.com", "qwe123!", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The employee id field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyContactNumber() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "", "Male", "a5@r.com", "qwe123", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The contact number field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyEmail() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "", "qwe123", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The email field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailInvalidEmail() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "asd!@#", "qwe123", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The email field must contain a valid email address.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmailExisted() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a1@r.com", "qwe123", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "This email already exist. Please choose another email.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyUsername() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a6@r.com", "", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The username field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailUsernameTooShort() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a7@r.com", "@", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "Your username is too short, minimum 6 characters required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailUsernameExisted() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "admin_example", "123456", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "This username already exist. Please choose another username.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyPassword() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The password field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailPasswordTooShort() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*", "1", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "Your password is too short, minimum 6 characters required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyOfficeShift() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "", "Admin Demo", "Manager", "Test Leader", "1000", "10", "3.jpg", "The office shift field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyRole() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "", "Manager", "Test Leader", "1000", "10", "3.jpg", "The staff role field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyDepartment() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "Admin Demo", "", "Test Leader", "1000", "10", "3.jpg", "The department field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyDesignation() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "Admin Demo", "QA", "", "1000", "10", "3.jpg", "The designation field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyBasicSalary() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "Admin Demo", "QA", "Software Tester", "", "10", "3.jpg", "The basic salary field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyHourlyRate() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "Admin Demo", "QA", "Software Tester", "123030", "", "3.jpg", "The hourly rate field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailEmptyProfilePicture() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "Admin Demo", "QA", "Software Tester", "123030", "100", "?", "The profile picture field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailNotImageFile() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "Admin Demo", "QA", "Software Tester", "123030", "100", "a.txt", "The profile picture field is required.");
+    }
+
+    @Test
+    public void testAddNewEmployeeFailProfilePictureFileSizeTooLarge() throws Exception {
+        loginPage = new LoginPage(driver);
+        loginPage.loginSuccess("admin_example", "123456");
+        Thread.sleep(Duration.ofSeconds(2));
+        homePage = new HomePage(driver);
+        homePage.navigateToEmployeesPage();
+        employeesPage = new EmployeesPage(driver);
+        employeesPage.addNewEmployee("first name", "last name", "?", "0", "Male", "a8@r.com", "asdzxc", "*****1", "2", "Admin Demo", "QA", "Software Tester", "123030", "100", "57mb.gif", "The profile picture field is required.");
+    }
+
     @AfterClass
     public void afterTest() {
         driver.quit();
